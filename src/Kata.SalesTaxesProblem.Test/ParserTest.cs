d5aaa5ac98af3cbce4d2c2a2d1c31d37de982111
CryptoBao <tizianof95@gmail.com>
2020-11-13 21:41:04 +0100

    Fix test case

--- src/Kata.SalesTaxesProblem.Test/ParserTest.cs
+++ src/Kata.SalesTaxesProblem.Test/ParserTest.cs
@@ -16,7 +16,7 @@ namespace Kata.SalesTaxesProblem.Test
 
     [TestCase("2 book at 12.49", 2, "book", 12.49, false)]
     [TestCase("100 papers at 5.99", 100, "papers", 5.99, false)]
-    [TestCase("0 chocolate at 0.00", 0, "zero", 0, false)]
+    [TestCase("0 chocolate at 0.00", 0, "chocolate", 0, false)]
     [TestCase("3 box of imported chocolates at 11.25", 3, "box of imported chocolates", 11.25, true)]
     [TestCase("1 imported bottle of perfume at 27.99", 1, "imported bottle of perfume", 27.99, true)]
     public void Parse_SingleGoods(string input, int expectedAmout, string expectedName, double expectedPrice, bool expectedIsImported)
