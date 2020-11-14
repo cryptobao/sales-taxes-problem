d8c2bb5b24078dd66b65c5c4474f56bdfe01c280
CryptoBao <tizianof95@gmail.com>
2020-11-14 11:31:41 +0100

    Implementa Parser

--- src/Kata.SalesTaxesProblem/Parser.cs
+++ src/Kata.SalesTaxesProblem/Parser.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Kata.SalesTaxesProblem.Domain;
 
 namespace Kata.SalesTaxesProblem
@@ -14,7 +15,25 @@ namespace Kata.SalesTaxesProblem
 
     public IEnumerable<Goods> Parse(IEnumerable<string> goodsStrings)
     {
-      throw new System.NotImplementedException();
+      return goodsStrings.Select(Parse);
+    }
+
+    public Goods Parse(string goodString)
+    {
+      var words = goodString.Split(' ');
+      var amount = int.Parse(words.First());
+      var price = double.Parse(words.Last());
+      var name = string.Join(' ', words.Skip(1).SkipLast(1).Where(x => x != "at"));
+      var isImported = name.Contains("imported");
+
+      return new Goods
+      {
+        Amout = amount,
+        Name = name,
+        Price = price,
+        IsImported = isImported
+      };
+
     }
 
   }
