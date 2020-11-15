5b1bc1713b9bff8c713e70658308f99680897155
CryptoBao <tizianof95@gmail.com>
2020-11-15 19:06:48 +0100

    Implementa CLI di base

--- src/Kata.SalesTaxesProblem/Program.cs
+++ src/Kata.SalesTaxesProblem/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Kata.SalesTaxesProblem
 {
@@ -6,7 +9,59 @@ namespace Kata.SalesTaxesProblem
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      if(!ArgsAreValid(args))
+      {
+        PrintSummary();
+        return;
+      }
+
+      var items = new List<string>();
+      var freeTax = new List<string>();
+
+      if(!TryReadFiles(args[0], ref items))
+      {
+        PrintFileNotValid(args[0]);
+        return;
+      }
+      else if(args.Length == 2 && !TryReadFiles(args[1], ref freeTax))
+      {
+        PrintFileNotValid(args[1]);
+        return;
+      }
+
+      var result = App.Instance(freeTax).Start(items);
+      result.ToList().ForEach(Console.WriteLine);
+
+    }
+
+
+    static void PrintSummary()
+    {
+      Console.WriteLine("You need to provide these arguments:");
+      Console.WriteLine("1. File path of what was purchased");
+      Console.WriteLine("2. (OPTIONAL) List of goods exempt from sales tax");
+    }
+
+    static void PrintFileNotValid(string path)
+    {
+      var oldColor = Console.ForegroundColor;
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine($"File not found {path}");
+      Console.ForegroundColor = oldColor;
+    }
+
+    static bool TryReadFiles(string path, ref List<string> output)
+    {
+      if(!File.Exists(path))
+        return false;
+
+      output.AddRange(File.ReadAllLines(path));
+      return true;
+    }
+
+    static bool ArgsAreValid(string[] args)
+    {
+      return args.Length != 0 && args.Length <= 2;
     }
   }
 }
